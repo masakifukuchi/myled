27d3f4c93ac41100d26109622484bb4397c32149
masakifukuchi <72370342+masakifukuchi@users.noreply.github.com>
2021-01-09 13:00:17 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -23,76 +23,76 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 	if(copy_from_user(&c,buf,sizeof(char)))                                                                     
 		return -EFAULT;
     
-  if(c == '0'){
-    gpio_base[10] = 1 << 25;                                                                            
-    gpio_base[10] = 1 << 24;                                                                            
-    gpio_base[10] = 1 << 21;                                                                    
-  }else if(c == '1'){                                                                                         
-    gpio_base[7] = 1 << 25;                                                                             
-    gpio_base[7] = 1 << 23;                                                                     
-  }else if(c == '2'){                                                                                       
-    for(i=0; i<10; i++){                                                                                        
-      gpio_base[7] = 1 << 25;                                                                             
-      msleep(250);                                                                                        
-      gpio_base[10] = 1 << 25;                                                                           
-      msleep(250);                                                                                
-    }                                                                                                   
-    gpio_base[7] = 1 << 24;                                                                             
-    for(i=0; i<2; i++){                                                                                 
-      ssleep(3);                                                                                          
-      gpio_base[10] = 1 << (23-i);                                                                        
-      gpio_base[7] = 1 << (22-i);                                                                 
-    }                                                                                           
-  }
-  return 1;                                                                                   
+  	if(c == '0'){
+		gpio_base[10] = 1 << 25;                                                                            
+    		gpio_base[10] = 1 << 24;                                                                            
+   		gpio_base[10] = 1 << 21;                                                                    
+  	}else if(c == '1'){                                                                                         
+    		gpio_base[7] = 1 << 25;                                                                             
+   		gpio_base[7] = 1 << 23;                                                                     
+  	}else if(c == '2'){                                                                                       
+    		for(i=0; i<10; i++){                                                                                        
+      			gpio_base[7] = 1 << 25;                                                                             
+      			msleep(250);                                                                                        
+      			gpio_base[10] = 1 << 25;                                                                           
+      			msleep(250);                                                                                
+    		}                                                                                                   
+    		gpio_base[7] = 1 << 24;                                                                             
+    		for(i=0; i<2; i++){                                                                                 
+      			ssleep(3);                                                                                          
+      			gpio_base[10] = 1 << (23-i);                                                                        
+      			gpio_base[7] = 1 << (22-i);                                                                 
+    		}                                                                                           
+  	}
+  	return 1;                                                                                   
 }
 
 static struct file_operations led_fops = {                                                                  
-  .owner = THIS_MODULE,                                                                               
-  .write =led_write,                                                                          
+  	.owner = THIS_MODULE,                                                                               
+  	.write =led_write,                                                                          
 };                                                                                                                                                                                                      
 
 static int __init init_mod(void)                                                                    
 {                                                                                                           
-  int retval;                                                                                         
-  retval = alloc_chrdev_region(&dev, 0, 1, "myled");                                                  
-  if(retval < 0){                                                                                             
-    printk(KERN_ERR "alloc_chrdev_region failed.\n");                                                   
-    return retval;                                                                              
-  }                                                                                                   
-  printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+  	int retval;                                                                                         
+  	retval = alloc_chrdev_region(&dev, 0, 1, "myled");                                                  
+  	if(retval < 0){                                                                                             
+    		printk(KERN_ERR "alloc_chrdev_region failed.\n");                                                   
+    		return retval;                                                                              
+  	}                                                                                                   
+  	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
   
-  cdev_init(&cdv, &led_fops);                                                                         
-  retval = cdev_add(&cdv, dev, 1);                                                                    
-  if(retval < 0){                                                                                             
-    printk(KERN_ERR "cdev_add failed. major:%d, minor:%d", MAJOR(dev), MINOR(dev));                     
-    return retval;                                                                              
-  }                                                                                                                                                                                                       
-  cls = class_create(THIS_MODULE, "myled");
-  if(IS_ERR(cls)){                                                                                            
-    printk(KERN_ERR "class_create failed.");                                                            
-    return PTR_ERR(cls);                                                                        
-  }                                                                                                   
-  device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));                                                                                                                                             
-  gpio_base = ioremap_nocache(0xfe200000, 0xA0);                                                                                                                                                          
+  	cdev_init(&cdv, &led_fops);                                                                         
+  	retval = cdev_add(&cdv, dev, 1);                                                                    
+  	if(retval < 0){                                                                                             
+   		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d", MAJOR(dev), MINOR(dev));                     
+    		return retval;                                                                              
+  	}                                                                                                                                                                                                       
+ 	cls = class_create(THIS_MODULE, "myled");
+  	if(IS_ERR(cls)){                                                                                            
+    		printk(KERN_ERR "class_create failed.");                                                            
+   		return PTR_ERR(cls);                                                                        
+  	}                                                                                                   
+  	device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));                                                                                                                                             
+  	gpio_base = ioremap_nocache(0xfe200000, 0xA0);                                                                                                                                                          
   
-  for( i=0; i<5; i++){                                                                                        
-    const u32 led = 25-i;                                                                              
-    const u32 index = led/10;                                                                           
-    const u32 shift = (led%10)*3; 
-    const u32 mask = ~(0x7 << shift);
-    gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
-  }                                                                                                   
-  return 0;
+  	for( i=0; i<5; i++){                                                                                        
+    		const u32 led = 25-i;                                                                              
+   		const u32 index = led/10;                                                                           
+    		const u32 shift = (led%10)*3; 
+    		const u32 mask = ~(0x7 << shift);
+    		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+  	}                                                                                                   
+  	return 0;
 }                                                                                                                                                                                                       
 
 static void __exit cleanup_mod(void)                                                               
 {                                                                                                          
-  cdev_del(&cdv);                                                                                     
-  device_destroy(cls, dev);                                                                           
-  class_destroy(cls);                                                                                
-  unregister_chrdev_region(dev, 1);                                                                  
-  printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));                         
+  	cdev_del(&cdv);                                                                                     
+  	device_destroy(cls, dev);                                                                           
+  	class_destroy(cls);                                                                                
+  	unregister_chrdev_region(dev, 1);                                                                  
+  	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));                         
 }                                                                                                                                                                                                      
 
 module_init(init_mod);                                                                             
